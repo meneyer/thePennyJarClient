a5d109a0810e7a19eb39dfd163c2e615684c0c41
Megan Neyer <Megan.Neyer@gmail.com>
2021-05-04 20:33:26 -0400

    remove https from environment file

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -7,7 +7,7 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:3000';
         break;
     // this is the deployed react application
-    case 'https://thepennyjarclient.herokuapp.com/':
+    case 'thepennyjarclient.herokuapp.com':
         // this is the full url of your deployed API
         APIURL = 'https://pennyjarserver.herokuapp.com/'
 }
